8d99ce79d519fbd6d36aa6f6cf1fbe670fa20280
parykhan-jameel <parexan.00443431@gmail.com>
2024-05-27 21:01:43 +0300

    displaying hexCode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 "use client"
 import ColorPicker from "@/components/next-iro/colorPicker"
 export default function Home() {
+  const handleColorChange = (color: string) =>{
+    console.log("selected color: ",color);
+  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-         <ColorPicker onColorChange={() => {}}/>
+      <h1>Pick a color</h1>
+         <ColorPicker onColorChange={handleColorChange}/>
     </main>
   );
 }
