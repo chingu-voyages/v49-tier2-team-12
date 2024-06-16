33d3af61aa827b54e651c8cf5bc0250fdfe63bd5
TiaDev7474 <nomenjanaharyriry@gmail.com>
2024-06-17 01:59:29 +0300

    fix clipboard error

--- src/app/app/(component)/color_pallette.tsx
+++ src/app/app/(component)/color_pallette.tsx
@@ -16,7 +16,7 @@ const ColorPalette = ({ colors }: ColorPaletteProps) => {
     )
     useEffect(() => {
         setCopy(false)
-    } , []);
+    } , [colors]);
     return (
         <div className="flex flex-wrap gap-4 p-4 bg-white shadow rounded-lg" >
             {colors.map((color, index) => (
@@ -30,16 +30,20 @@ const ColorPalette = ({ colors }: ColorPaletteProps) => {
             <div className="flex items-center justify-center">
                 <CopyToClipboard
                     text={JSON.stringify(colorsJson)}
-                    onCopy={() => setCopy(true)}
+                    onCopy={() => {
+                        setCopy(true)
+                    }}
+
                 >
                     {
                         copy ? (
-                            <span className="text-green-500">
+                            <span className="text-green-500 text-sm flex  justify-center items-center gap-2">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
                                      className="lucide lucide-clipboard-check"><rect width="8" height="4" x="8" y="2" rx="1" ry="1"/>
                                     <path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"/>
                                     <path d="m9 14 2 2 4-4"/>
                                 </svg>
+                                <p>copied!</p>
                             </span>
                         ): (
                             <button className="p-3 text-blue-300 hover:text-blue-700 bg-gray-200 hover:bg-opacity-50 rounded-lg">
